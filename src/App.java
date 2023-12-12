c995f6bf975ec85e118072d89691bc9efae084f8
FS-Baz <s443011919@st.uqu.edu.sa>
2023-12-12 19:15:27 +0300

    1st Commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,38 @@
-public class App {
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.time.Clock;
+import java.time.LocalTime;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        launch(args);
+   
+    }
+
+    @Override
+    public void start(Stage PrimaryStage) throws Exception {
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("MyClockController.fxml"));
+            Scene scene = new Scene(root);
+
+
+
+            PrimaryStage.setTitle("Clock");
+            PrimaryStage.setScene(scene);
+            PrimaryStage.show();
+        } catch (Exception e) {
+          
+        }
+            
+
+        
+        
     }
 }
---
+++ src/MyClockController.java
@@ -0,0 +1,45 @@
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.util.Duration;
+
+public class MyClockController implements Initializable {
+
+    @FXML
+    private Label ClockText;
+
+
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+       Timeline timeline = new Timeline(
+        new KeyFrame(Duration.seconds(1.0), e -> {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss a");
+        Date date = new Date();
+        ClockText.setText(sdf.format(date));
+    
+    })
+        );
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
+
+
+    
+
+
+}
